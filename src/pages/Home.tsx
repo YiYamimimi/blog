01434b5993d871faccd51f9966586897b8d349e0
yiyamimimi <483916945@qq.com>
2026-04-11 12:13:54 +0800

    项目跳转

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <div className="max-w-[88vw] mx-auto xl:px-6 p-2 -mt-35">
         <div className="grid gap-8 xl:gap-20 mb-12 xl:mb-20 grid-cols-1 xl:grid-cols-[1.2fr_1fr] xl:items-center">
           <CardContent
-            onCardClick={() => window.open("http://47.107.143.203:3000/", "_blank")}
+            onCardClick={() => window.open("http://106.12.6.136", "_blank")}
           />
 
           <BlogListCard
